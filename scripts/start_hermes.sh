399e607a4db10aaf16a13e9eb619321bd0193eac
lesterli <fulfilself@gmail.com>
2023-04-28 11:53:16 +0800

    scripts for ics local test

---
+++ scripts/start_hermes.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+set -eux
+
+# Setup Hermes in packet relayer mode
+killall hermes 2> /dev/null || true
+
+tee ~/.hermes/config.toml<<EOF
+[global]
+log_level = "trace"
+[mode]
+[mode.clients]
+enabled = true
+refresh = true
+misbehaviour = true
+[mode.connections]
+enabled = true
+[mode.channels]
+enabled = true
+[mode.packets]
+enabled = true
+[[chains]]
+account_prefix = "oct"
+clock_drift = "5s"
+gas_adjustment = 0.1
+grpc_addr = "tcp://${CONSUMER_GRPC_ADDR}"
+id = "$CONSUMER_CHAIN_ID"
+key_name = "relayer"
+max_gas = 2000000
+rpc_addr = "http://${CONSUMER_RPC_LADDR}"
+rpc_timeout = "10s"
+store_prefix = "ibc"
+trusting_period = "599s"
+websocket_addr = "ws://${CONSUMER_RPC_LADDR}/websocket"
+[chains.gas_price]
+       denom = "stake"
+       price = 0.00
+[chains.trust_threshold]
+       denominator = "3"
+       numerator = "1"
+[[chains]]
+account_prefix = "cosmos"
+clock_drift = "5s"
+gas_adjustment = 0.1
+grpc_addr = "tcp://${PROVIDER_GRPC_ADDR}"
+id = "$PROVIDER_CHAIN_ID"
+key_name = "relayer"
+max_gas = 2000000
+rpc_addr = "http://${PROVIDER_RPC_LADDR}"
+rpc_timeout = "10s"
+store_prefix = "ibc"
+trusting_period = "599s"
+websocket_addr = "ws://${PROVIDER_RPC_LADDR}/websocket"
+[chains.gas_price]
+       denom = "stake"
+       price = 0.00
+[chains.trust_threshold]
+       denominator = "3"
+       numerator = "1"
+EOF
+
+# Delete all previous keys in relayer
+hermes keys delete $CONSUMER_CHAIN_ID -a
+hermes keys delete $PROVIDER_CHAIN_ID -a
+
+# Restore keys to hermes relayer
+hermes keys restore --mnemonic "$(jq -r .mnemonic $CONSUMER_HOME/consumer_keypair.json)" $CONSUMER_CHAIN_ID
+# temp_start_provider.sh creates key pair and stores it in keypair.json
+hermes keys restore --mnemonic "$(jq -r .mnemonic $PROVIDER_HOME/keypair.json)" $PROVIDER_CHAIN_ID
+
+sleep 5
+
+hermes create connection $CONSUMER_CHAIN_ID --client-a 07-tendermint-0 --client-b 07-tendermint-0
+hermes create channel $CONSUMER_CHAIN_ID --port-a consumer --port-b provider -o ordered --channel-version 1 connection-0
+
+sleep 1
+
+hermes -j start &> ~/.hermes/logs &
+
+############################################################
+
+PROVIDER_VALIDATOR_ADDRESS=$(jq -r .address $PROVIDER_HOME/keypair.json)
+DELEGATIONS=$($PROVIDER_BINARY q staking delegations $PROVIDER_VALIDATOR_ADDRESS --home $PROVIDER_HOME --node tcp://${PROVIDER_RPC_LADDR} -o json)
+OPERATOR_ADDR=$(echo $DELEGATIONS | jq -r .delegation_responses[0].delegation.validator_address)
+
+./$PROVIDER_BINARY tx staking delegate $OPERATOR_ADDR 50000000stake \
+       --from $VALIDATOR \
+       $KEYRING \
+       --home $PROVIDER_HOME \
+       --node tcp://${PROVIDER_RPC_LADDR} \
+       --chain-id $PROVIDER_CHAIN_ID -y -b block
+sleep 1
